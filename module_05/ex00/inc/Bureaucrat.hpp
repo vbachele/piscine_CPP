d0d33100275471203e93c687baf5a908f04a1d70
Vincent <vincent@MacBook-Air-de-Vincent.local>
2022-03-24 13:54:59 +0100

    début ex00

--- module_05/ex00/inc/Bureaucrat.hpp
+++ module_05/ex00/inc/Bureaucrat.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vincent <vincent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:15:21 by vbachele          #+#    #+#             */
-/*   Updated: 2022/03/23 18:16:49 by vbachele         ###   ########.fr       */
+/*   Updated: 2022/03/24 13:18:09 by vincent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,18 @@
 
 class Bureaucrat{
 public:
+	Bureaucrat;
+	~Bureaucrat;
+	void	GradeTooHighException();
+	void	GradeTooLowException();
+	void	getGrade();
+	void	getGrade();
+	void	incrementGrade();
+	void	DecerementGrade();
 
 private:
-
+	std::string _name;
+	int			_grade;
 };
 
 #endif
--- module_05/ex00/src/Bureaucrat.cpp
+++ module_05/ex00/src/Bureaucrat.cpp
@@ -3,12 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vincent <vincent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:17:15 by vbachele          #+#    #+#             */
-/*   Updated: 2022/03/23 18:17:34 by vbachele         ###   ########.fr       */
+/*   Updated: 2022/03/24 13:20:06 by vincent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+
+/***************** Canonical form **************/
+
+Bureaucrat::Bureaucrat()
+{
+	std::cout << "Constructor is called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Destructor is called" << std::endl;
+}
+
+/***************** Member functions **************/
+
+void	Bureaucrat::getName()
+{
+	return (this->_name);
+}
+
+void	Bureaucrat::getGrade()
+{
+	return (this->_grade);
+}
+void	Bureaucrat::GradeTooHighException()
+{
+	std::cout << "You can't this grade is too high" << std::endl;
+}
+
+void	Bureaucrat::GradeTooLowException();
+{
+	std::cout << "You can't this grade is too low" << std::endl;	
+}
+
+void	
