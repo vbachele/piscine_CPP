9a7079c02d3b4e0c767d5b1d6c0e70711f56d828
vincentbachelet-collab <vcbachelet@gmail.com>
2022-03-29 15:50:31 +0200

    prout

--- module_05/ex00/inc/Bureaucrat.hpp
+++ module_05/ex00/inc/Bureaucrat.hpp
@@ -6,12 +6,13 @@
 /*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:15:21 by vbachele          #+#    #+#             */
-/*   Updated: 2022/03/23 18:16:49 by vbachele         ###   ########.fr       */
+/*   Updated: 2022/03/29 15:49:34 by vbachele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_H
 # define BUREAUCRAT_H
+#include "iostream"
 
 class Bureaucrat{
 public:
--- module_05/ex00/src/Bureaucrat.cpp
+++ module_05/ex00/src/Bureaucrat.cpp
@@ -6,9 +6,38 @@
 /*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:17:15 by vbachele          #+#    #+#             */
-/*   Updated: 2022/03/23 18:17:34 by vbachele         ###   ########.fr       */
+/*   Updated: 2022/03/29 15:46:25 by vbachele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+/************* canonical form ***************/
+
+Bureaucrat::Bureaucrat()
+{
+	std::cout << "Bureaucrat constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &old_obj)
+{
+	std::cout << "copy Bureaucrat constructor called" << std::endl;
+	*this = old_obj;
+}
+
+// Here i copy all the idea from the Bureaucrat of the previous obj
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &obj)
+{
+	this->obj = obj;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat destructor called" << std::endl;
+}
+
+// +------------------------------------------+ //
+//   				Member functions		 //
+// +------------------------------------------+ //
